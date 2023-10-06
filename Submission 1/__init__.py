e5466d514ef0918e5279184f4417380a4bbfea05
Anders Karlskås <anders@karlskaas.no>
2023-10-06 11:37:58 +0200

    Added pictures, changed graphics, +++

--- Submission 1/__init__.py
+++ Submission 1/__init__.py
@@ -6,83 +6,35 @@ from PIL import Image
 from flask import render_template, redirect, url_for, flash
 from forms import FileForm
 from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+
 import matplotlib.pyplot as plt
+# from matplotlib.figure import Figure
 import numpy as np
 import cv2
 
 
 from baseconfig import app
 
-def cluster_this(image_data):
 
-    decoded_image = np.frombuffer(b64decode(image_data), dtype=np.uint8)
-    img = cv2.imdecode(decoded_image, cv2.COLOR_BGR2RGB)
-    
-    # Splitting into channels
-    blue, green, red = cv2.split(img)
-    
-    df_blue = blue/255
-    df_green = green/255
-    df_red = red/255
-
-    pca_b = PCA(n_components=50)
-    pca_b.fit(df_blue)
-    trans_pca_b = pca_b.transform(df_blue)
-    pca_g = PCA(n_components=50)
-    pca_g.fit(df_green)
-    trans_pca_g = pca_g.transform(df_green)
-    pca_r = PCA(n_components=50)
-    pca_r.fit(df_red)
-    trans_pca_r = pca_r.transform(df_red)
-
-    print(f"Blue Channel : {sum(pca_b.explained_variance_ratio_)}")
-    print(f"Green Channel: {sum(pca_g.explained_variance_ratio_)}")
-    print(f"Red Channel  : {sum(pca_r.explained_variance_ratio_)}")
-
-    img_array = []
-
-    fig_1 = plt.figure(figsize = (15, 7.2))
-    plt.title("Blue Channel")
-    plt.ylabel('Variation explained')
-    plt.xlabel('Eigen Value')
-    plt.bar(list(range(1,51)),pca_b.explained_variance_ratio_)
-
-    buf = BytesIO()
-    plt.savefig(buf, format='png', dpi=80)
-    buf.seek(0)
-    image_data = np.array(bytearray(buf.read()), dtype=np.uint8)
-    img_array.append(cv2.imdecode(image_data, cv2.IMREAD_COLOR))
-
-    fig_2 = plt.figure(figsize = (15, 7.2))
-    plt.title("Green Channel")
-    plt.ylabel('Variation explained')
-    plt.xlabel('Eigen Value')
-    plt.bar(list(range(1,51)),pca_g.explained_variance_ratio_)
-
-    buf = BytesIO()
-    plt.savefig(buf, format='png', dpi=80)
-    buf.seek(0)
-    image_data = np.array(bytearray(buf.read()), dtype=np.uint8)
-    img_array.append(cv2.imdecode(image_data, cv2.IMREAD_COLOR))
-
-    fig_3 = plt.figure(figsize = (15, 7.2))
-    plt.title("Red Channel")
-    plt.ylabel('Variation explained')
-    plt.xlabel('Eigen Value')
-    plt.bar(list(range(1,51)),pca_r.explained_variance_ratio_)
-
-    buf = BytesIO()
-    plt.savefig(buf, format='png', dpi=80)
-    buf.seek(0)
-    image_data = np.array(bytearray(buf.read()), dtype=np.uint8)
-    img_array.append(cv2.imdecode(image_data, cv2.IMREAD_COLOR))
-
-    return [img_array]
-
+def load_images_from_folder(folder_path):
+    dataset = []
+    names = []
+    filepath = os.path.dirname(__file__) + folder_path
+    for filename in os.listdir(filepath):
+        if filename.endswith(".jpg"):
+            img_path = os.path.join(filepath, filename)
+            img = cv2.imread(img_path)
+            if img is not None:
+                # Resize the image to a common size if needed
+                img = cv2.resize(img, (200, 100))
+                img = img.reshape(-1)
+                dataset.append(img)
+                names.append(filename)
+    return np.array(dataset), names
 
 
 def decode_img(form_data: FileForm) -> list:
-
     img = Image.open(form_data)
     data = BytesIO()
     img.save(data, "PNG")
@@ -91,7 +43,6 @@ def decode_img(form_data: FileForm) -> list:
     decoded_img = encoded_img.decode('utf-8')
     
     return encoded_img, decoded_img
-
 
 
 @app.route("/", methods=["GET"])
@@ -103,20 +54,57 @@ def clustering():
     form = FileForm()
     if form.submit.data and form.validate():
     
+        folder = form.folder.data
+        data, names = load_images_from_folder(f"/static/photos/clustering/{folder}")
 
-        image =  decode_img(form.file_dump.data)    
-        uri_original = f"data:image/png;base64,{image[1]}"
-        cluster_images = cluster_this(image[0])
-        
-        return render_template("clustering.html", form=form, uri_original=uri_original, cluster_images=cluster_images)
+        # Folder path where JPG pictures are stored
+        folder_path = "bilder"
+        data, names = load_images_from_folder(folder_path)
+
+        # Apply PCA to reduce dimensionality
+        num_components = len(names)  # You can adjust this number based on your needs
+        pca = PCA(n_components=num_components)
+        data_pca = pca.fit_transform(data)
+
+        # Perform clustering (K-Means in this example)
+        num_clusters = 3  # You can adjust the number of clusters
+        kmeans = KMeans(n_clusters=num_clusters)
+        labels = kmeans.fit_predict(data_pca)
+
+        # Visualize the clustered data
+        plt.figure(figsize= (8, 6))
+        colors = ["r", "b", "g"]
+        for i in range(num_clusters):
+            plt.scatter(data_pca[labels == i, 0], data_pca[labels == i, 1], c=colors[i % len(colors)], label=f"Cluster {i+1}")
+
+        plt.title("PCA and K-Means Clustering")
+        plt.legend()
+        plt.xlabel("Principal Component 1")
+        plt.ylabel("Principal Component 2")
 
 
-    return render_template("clustering.html", form=form, uri_original='')
+        return render_template("clustering.html", form=form, uri_original='', cluster_images = '')
+
+        # image =  decode_img(form.file_dump.data)
+
+        # uri_original = f"data:image/png;base64,{image[1]}"
+
+        # cluster_images = cluster_this(image[0])
+        # return render_template("clustering.html", form=form, uri_original=uri_original, cluster_images=cluster_images)
+
+    return render_template("clustering.html", form=form, uri_original='', cluster_images = '')
+
+
 
 
 @app.route("/segmentation", methods=["POST", "GET"])
 def segmentation():
     return render_template("segmentation.html")
+
+
+@app.route("/risikoanalyse", methods=["POST", "GET"])
+def risikoanalyse():
+    return render_template("risikoanalyse.html")
 
 
 
--- Submission 1/forms.py
+++ Submission 1/forms.py
@@ -1,11 +1,14 @@
+import os
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
-from wtforms import SubmitField, StringField
+from wtforms import SubmitField, StringField, SelectField
 from wtforms.validators import DataRequired
 
 
 class FileForm(FlaskForm):
     # title = StringField("Title", validators=[DataRequired()], render_kw={"autofocus": True, "placeholder": ""})
-    file_dump = FileField("Test", validators=[FileRequired(), FileAllowed(["png", "jpg", "mp3", "jpeg"])], render_kw={"placeholder": ""})
-    submit = SubmitField("Upload file")
+    
+    folder_choices = [(folder, folder) for folder in os.listdir('static/photos/clustering') if os.path.isdir(os.path.join('static/photos/clustering', folder))]
+    folder = SelectField("Select a dataset", choices=folder_choices)
+    submit = SubmitField("Select")
